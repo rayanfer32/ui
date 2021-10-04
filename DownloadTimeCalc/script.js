61f24a8415667316c5349940c6409e914067ca77
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2021-10-04 14:37:44 +0530

    ✨ Added download time calculator

---
+++ DownloadTimeCalc/script.js
@@ -0,0 +1,29 @@
+const speedElm = document.querySelector("#speed");
+const filesizeElm = document.querySelector("#filesize");
+const calculateBtnElm = document.querySelector("#calculateBtn");
+const resultElm = document.querySelector("#result");
+
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+}
+
+function calculateTime(){
+    let res = filesizeElm.value*1024 / (speedElm.value/8);
+    resultElm.innerText = res.toString().toHHMMSS()
+}
+
+//attach event listners for changes in the input and autocalculate the result
+speedElm.addEventListener("input", calculateTime)
+filesizeElm.addEventListener("input", calculateTime)
+
+calculateBtnElm.addEventListener("click", calculateTime);
+calculateTime();
+// const autoCalcInterval = setInterval(calculateTime, 1000);
