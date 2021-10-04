fd60512bbf5f9e2ff16e70186dcd32cd94b389de
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2021-10-04 15:27:21 +0530

    🐛 Fixed bug: Check for NaN

--- DownloadTimeCalc/script.js
+++ DownloadTimeCalc/script.js
@@ -3,29 +3,40 @@ const filesizeElm = document.querySelector("#filesize");
 const calculateBtnElm = document.querySelector("#calculateBtn");
 const resultElm = document.querySelector("#result");
 const speedBtnsElm = document.querySelector("#speed-btns")
+const filesizeBtnsElm = document.querySelector("#filesize-btns")
 
 String.prototype.toHHMMSS = function () {
     var sec_num = parseInt(this, 10); // don't forget the second param
-    var hours   = Math.floor(sec_num / 3600);
+    var hours = Math.floor(sec_num / 3600);
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
     var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    return hours+':'+minutes+':'+seconds;
+    if (hours < 10) { hours = "0" + hours; }
+    if (minutes < 10) { minutes = "0" + minutes; }
+    if (seconds < 10) { seconds = "0" + seconds; }
+    return hours + ':' + minutes + ':' + seconds;
 }
 
-function calculateTime(){
-    let res = filesizeElm.value*1024 / (speedElm.value/8);
+function calculateTime() {
+    let res = filesizeElm.value * 1024 / (speedElm.value / 8);
     resultElm.innerText = res.toString().toHHMMSS()
 }
 
 speedBtnsElm.addEventListener("click", e => {
-    speedElm.value = e.target.value;
-    calculateTime()
+    let targetValue = e.target.value;
+    if (!isNaN(targetValue)) {
+        speedElm.value = targetValue;
+        calculateTime()
+    }
 })
 
+filesizeBtnsElm.addEventListener("click", e => {
+    let targetValue = e.target.value;
+    if (!isNaN(targetValue)) {
+        filesizeElm.value = targetValue;
+        calculateTime()
+    }
+})
 
 
 //attach event listners for changes in the input and autocalculate the result
